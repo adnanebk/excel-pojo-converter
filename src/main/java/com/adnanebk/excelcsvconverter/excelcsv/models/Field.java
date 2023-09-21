e8110fd7c6888ba7f3ba76819455d9a0a4fc39c7
adnane <adnane.benkouider@capgemini.com>
2023-09-21 18:16:47 +0100

    refactor

--- src/main/java/com/adnanebk/excelcsvconverter/excelcsv/models/Field.java
+++ src/main/java/com/adnanebk/excelcsvconverter/excelcsv/models/Field.java
@@ -11,13 +11,12 @@ public record Field<T>(String name, Class<?> type, String title, Method getter, 
         try {
             if(enumValues.length==0)
                 return getter.invoke(obj);
-            Object value = getter.invoke(obj);
-            String enumValue = getEnumValue(value);
-            return enumValue.isEmpty()?value:enumValue;
+            return getEnumValue(obj);
         } catch (IllegalAccessException | InvocationTargetException | IndexOutOfBoundsException e) {
             throw new ReflectionException(e.getMessage());
         }
     }
+
     public void setValue(Object obj,Object value){
         try {
             if(value==null || value.toString().isEmpty())
@@ -29,7 +28,12 @@ public record Field<T>(String name, Class<?> type, String title, Method getter, 
             throw new ReflectionException(e.getMessage());
         }
     }
-
+    private Object getEnumValue(T obj) throws IllegalAccessException, InvocationTargetException {
+        Object value = getter.invoke(obj);
+        int ordinal = ((Enum<?>) value).ordinal();
+        String enumValue = ordinal<enumValues.length?enumValues[ordinal]:"";
+        return enumValue.isEmpty() ? value : enumValue;
+    }
     private void setEnumValue(Object obj, Object value) throws InvocationTargetException, IllegalAccessException {
         try {
            setter.invoke(obj,Enum.valueOf(type.asSubclass(Enum.class), value.toString().toUpperCase().trim()));
@@ -47,12 +51,6 @@ public record Field<T>(String name, Class<?> type, String title, Method getter, 
         }
         throw new ReflectionException("Unknown or invalid enum value {"+value.toString()+"}");
     }
-
-    private String getEnumValue(Object value) {
-        int ordinal = ((Enum<?>) value).ordinal();
-        return ordinal<enumValues.length?enumValues[ordinal]:"";
-    }
-
 
     @Override
     public boolean equals(Object o) {
