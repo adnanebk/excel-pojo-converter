b8e90babe9d888cc2776a5ba7e53bbd6b7ab8646
adnane <adnane.benkouider@capgemini.com>
2023-10-07 22:31:11 +0100

    support boolean formatting

---
+++ src/main/java/com/adnanebk/excelcsvconverter/excelcsv/annotations/CellBoolean.java
@@ -0,0 +1,14 @@
+package com.adnanebk.excelcsvconverter.excelcsv.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface CellBoolean {
+
+    String trueValue();
+    String falseValue();
+}
